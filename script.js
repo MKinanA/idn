b26c8fade646e3fb30d46e989e8eb3fdb3ab7c11
MKinanA <mkinanahsan@gmail.com>
2023-11-28 13:33:08 +0700

    modified welcome_screen and nav behaviour

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-const body = document.body, welcome_screen = document.getElementById('welcome-screen'), welcome_screen_img = document.querySelector('#welcome-screen img'), main = document.querySelector('main'), welcome_screen_loading_circle = document.querySelector('#welcome-screen .loading-circle');
+const body = document.body, 
+welcome_screen = document.getElementById('welcome-screen'), 
+welcome_screen_img = document.querySelector('#welcome-screen img'), 
+main = document.querySelector('main'), 
+welcome_screen_loading_circle = document.querySelector('#welcome-screen .loading-circle'), 
+nav_links = document.querySelectorAll('header nav .link .dropdown a'), 
+background = document.getElementById('background');
 let window_loaded = false;
 
 window.onload = _ => {
@@ -10,11 +16,18 @@ window.onload = _ => {
         welcome_screen_img.style.transform = 'scale(100%)';
         setTimeout(function() {
             welcome_screen.classList.add('disappear');
-            body.style.overflowY = 'scroll';
             main.style.top = '0';
             setTimeout(function() {
                 main.style.transition = 'all 0.25s ease-out';
+                body.style.overflowY = 'scroll';
+                welcome_screen.style.display = 'none';
             }, 1000);
         }, 1000);
     }, 500);
 };
+
+nav_links.forEach(link => {
+    link.onclick = _ => {
+        document.getElementById(link.getAttribute('to')).scrollIntoView();
+    };
+});
