0d25d1bf203f783ad255349204582f1cb0c7739a
MKinanA <mkinanahsan@gmail.com>
2023-11-29 09:12:13 +0700

    updates 291120230912

--- script.js
+++ script.js
@@ -4,10 +4,17 @@ welcome_screen_img = document.querySelector('#welcome-screen img'),
 main = document.querySelector('main'), 
 welcome_screen_loading_circle = document.querySelector('#welcome-screen .loading-circle'), 
 nav_links = document.querySelectorAll('header nav .link .dropdown a'), 
-background = document.getElementById('background');
+background = document.getElementById('background'),
+btns = document.querySelectorAll('button'),
+btn_links = {
+    'daftar': 'https://psb.idn.sch.id/'
+},
+intro_sound = new Audio('attributes/intro_(windows_xp_startup_sound).mp3');
 let window_loaded = false;
+intro_sound.preload = 'auto';
 
 window.onload = _ => {
+    intro_sound.play();
     window_loaded = true;
     welcome_screen_loading_circle.style.display = 'none';
     welcome_screen_img.style.display = 'initial';
@@ -29,5 +36,11 @@ window.onload = _ => {
 nav_links.forEach(link => {
     link.onclick = _ => {
         document.getElementById(link.getAttribute('to')).scrollIntoView();
+    };
+});
+
+btns.forEach(btn => {
+    if (btn.getAttribute('for') in btn_links) {
+        btn.onclick = _ => window.open(btn_links[btn.getAttribute('for')], '_blank');
     };
 });
